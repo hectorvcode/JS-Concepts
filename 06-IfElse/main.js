dfeda321b11527e945c4e3b4284bf1fa73c306a3
HectorV <hectorfabiovasquez@gmail.com>
2024-03-04 21:47:28 -0500

    Added Else If Chapter

---
+++ 06-IfElse/main.js
@@ -0,0 +1,12 @@
+const x = 5;
+
+if (x === 5) {
+  console.log("This is true");
+} else {
+  console.log("Else statement");
+}
+
+console.log("I am here");
+
+/* if (x === 5) console.log("this is true");
+console.log("I am here"); */
