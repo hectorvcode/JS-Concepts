3d2d6a0b311b2151de195d1ce678cad758e7aa8d
HectorV <hectorfabiovasquez@gmail.com>
2024-03-04 22:29:47 -0500

    Added Switch module

---
+++ 09-Switch/main.js
@@ -0,0 +1,16 @@
+const title = "asdfasdf";
+
+switch (title) {
+  case "Junior":
+    console.log("You are Junior");
+    break;
+  case "Middle":
+    console.log("You are Middle");
+    break;
+  case "Senior":
+    console.log("You are Senior");
+    break;
+  default:
+    console.log("Sorry, I don't know who you are");
+    break;
+}
