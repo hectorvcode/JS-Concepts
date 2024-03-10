0d4836daada4b3365baeed53f987bdbf44fc7d1f
HectorV <hectorfabiovasquez@gmail.com>
2024-03-09 19:11:28 -0500

    Added Math module

---
+++ 22-Math/main.js
@@ -0,0 +1,30 @@
+//let num = 2.7
+let num = 2.4;
+
+/**
+ * Math.ceil()
+ */
+
+console.log(Math.ceil(num));
+
+/**
+ * Math.floor()
+ */
+
+console.log(Math.floor(num));
+
+/**
+ * Math.round
+ */
+
+console.log(Math.round(num));
+
+/**
+ * Math.random() function returns a floating-point
+ * pseudo-random number that's greater than or equal to 0 and less than 1
+ * with approximately uniform distribution over that range - which you can then scale to your desired range
+ * The implementation selects the initial seed to the random number generation algorithm
+ * it cannot be chosen or reset by the user
+ */
+
+console.log(Math.round(Math.random() * 10));
