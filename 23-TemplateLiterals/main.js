5f271d56adb5e3b94b48dd554b27fc61fffc2161
HectorV <hectorfabiovasquez@gmail.com>
2024-03-09 21:42:40 -0500

    Added Template Literals module and JavaScript array module

---
+++ 23-TemplateLiterals/main.js
@@ -0,0 +1,11 @@
+const firstName = "John";
+const lastName = "Doe";
+const age = 25;
+
+//My name is John Doe and I am 25 years old
+
+//String concatenation
+console.log("My name is " + firstName + " " + lastName + " " + "and I am " + age + " years old")
+
+//template strings
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old`)
