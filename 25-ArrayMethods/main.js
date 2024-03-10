7eb37e7450e6474534c0113063dfabb86d3e2f7e
HectorV <hectorfabiovasquez@gmail.com>
2024-03-10 18:22:15 -0500

    Added Array Methods module

--- 25-ArrayMethods/main.js
+++ 25-ArrayMethods/main.js
@@ -61,4 +61,75 @@ console.log("secord unshift", arr3);
 /**
  * shift()
  */
+const arr4 = [1, 2, 3, 4, 5];
+console.log("shift", arr4.shift());
+console.log("arr4", arr4);
+
+/**
+ * forEach()
+ */
+
+const arrayOfName = ["Alice", "Jack", "John", "Jane", "Bob"];
+
+arrayOfName.forEach((name) => console.log(name));
+
+arrayOfName.forEach((name, index) => {
+  return console.log(`Element index: ${index} - Element value: ${name}`);
+});
+
+arrayOfName.forEach((name, index, arr) => {
+  return console.log(`Index: ${index} - Value: ${name} - Array: ${arr}`);
+});
+
+/**
+ * slice()
+ */
+
+const animals1 = ["ant", "bison", "camel", "duck", "elephant"];
+const lastThreeElements = animals1.slice(2);
+console.log(lastThreeElements);
+
+//end index excludes the element
+
+const betweenElements = animals1.slice(2, 4);
+console.log(betweenElements);
+
+const negativeIndex = animals1.slice(-2);
+console.log(negativeIndex);
+
+/**
+ * splice()
+ */
+
+//Adding one element to the array
+const animals2 = ["ant", "bison", "elephant"];
+const camel = "camel";
+/* animals2.splice(1, 0, camel);
+console.log(animals2); */
+
+//Using splice with delete
+/* animals2.splice(1,1,camel)
+console.log(animals2) */
+
+//Adding multiple elements
+/* const cat = "cat";
+animals2.splice(1, 0, camel, cat);
+console.log(animals2); */
+
+//No elements specified
+animals2.splice(1, 2);
+console.log(animals2);
+
+/**
+ * reverse()
+ */
+
+const animals3 = ["ant", "bison", "camel", "duck", "elephant"];
+const animalsReverse = animals3.reverse();
+console.log(`Initial array: [${animals3}]`);
+console.log(`Reverse array: [${animalsReverse}]`);
+
+/**
+ * sort()
+ */
 
