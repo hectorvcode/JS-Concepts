f2959d3c1936768342182786fbbf34ed904640b6
HectorV <hectorfabiovasquez@gmail.com>
2024-03-14 06:55:43 -0500

    Added JavaScript Objects Module

---
+++ 27-JavaScriptObjects/main.js
@@ -0,0 +1,20 @@
+const john = {
+  name: "John",
+  age: 25,
+};
+
+console.log(john);
+
+//dot notation
+console.log(john.name);
+
+//bracket notation
+console.log(john["age"]);
+
+//add property
+john.language = "JS";
+console.log(john);
+
+//remove property
+delete john.age;
+console.log(john);
