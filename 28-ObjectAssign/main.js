194cc6533d81ca9d1a29a8c7a0a4afda20555027
HectorV <hectorfabiovasquez@gmail.com>
2024-04-10 08:33:00 -0500

    Added examples from the course

--- 28-ObjectAssign/main.js
+++ 28-ObjectAssign/main.js
@@ -1,3 +1,10 @@
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+const returnedTarget = Object.assign(target, source);
+console.log(target);
+console.log(returnedTarget);
+
+//video content
 const john = {
   name: "John",
   knowJava: true,
--- 29-ObjectCreate/main.js
+++ 29-ObjectCreate/main.js
@@ -1,3 +1,15 @@
+const options = {
+  width: 1024,
+  height: 1024,
+  name: "test",
+};
+
+const optionsSecond = Object.create(options);
+optionsSecond.width = 1200;
+console.log("options", options);
+console.log("optionSecond Width", optionsSecond.width);
+console.log("options Width", options.width);
+
 const coder = {
   code() {
     console.log(`${this.name} is coding`);
